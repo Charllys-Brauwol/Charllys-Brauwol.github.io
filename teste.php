fbe29c560e74ed43a024309a387ae1686c3f0362
Charllys Brauwol <charllysbrauwolhaha@gmail.com>
2025-05-31 12:24:35 -0300

    Add files via upload

---
+++ teste.php
@@ -0,0 +1,8 @@
+<?php 
+
+$num1 = 3;
+$num2 = 5;
+
+echo "A soma de $num1 e $num2 é: " . ($num1 + $num2);
+
+?>
---
+++ testesbd.sql
@@ -0,0 +1,18 @@
+
+
+
+
+
+INSERT INTO exemplo (nome, idade, data_nascimento)
+VALUES ('Pedro Sousa', 40, '1985-11-09');
+
+
+
+
+
+
+
+
+
+
+
